82ed17944edf041017d75d4ab28ca11963f7adc1
akedanai Jonganan <mc@akedanais-Mac-mini.local>
2024-06-10 12:14:10 +0700

    update

--- k8s/phpdemo/src/index.php
+++ k8s/phpdemo/src/index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
 
 <h2>
@@ -10,6 +15,6 @@ echo $_SERVER['SERVER_SOFTWARE']. "<br>";
 echo(rand() . "<br>");
 ?>
 </h>
-
+    
 </body>
 </html>
