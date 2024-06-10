331ed9bf4204175083aa73027fdc30d6b2e304b9
akedanai Jonganan <mc@akedanais-Mac-mini.local>
2024-06-10 11:41:12 +0700

    add dokcerfile

---
+++ k8s/phpdemo/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>
+<?php
+echo $_SERVER['SERVER_NAME']. "<br>";
+echo $_SERVER['SERVER_ADDR']. "<br>";
+echo $_SERVER['SERVER_SOFTWARE']. "<br>";
+echo(rand() . "<br>");
+?>
+</h>
+
+</body>
+</html>
